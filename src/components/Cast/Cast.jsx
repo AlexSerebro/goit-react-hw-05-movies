9cb7e959bdf4dc0af46762ce49ffc7e83077605e
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-05-30 19:56:43 +0300

    add actor card

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,1 +1,8 @@
-import {useState, useEffect}
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieCast } from '../../services/movies-api';
+import ActorCard from "../ActorCard";
+import s from "./Cast.module.css";
+
+export function
+
